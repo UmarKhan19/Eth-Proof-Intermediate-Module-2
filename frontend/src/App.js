37c665bad3dfa8b270c733eac9b39dbb72c15945
UmarKhan19 <khanumar0108@gmail.com>
2023-07-14 14:56:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [contract, setContract] = useState("");
   const abi = UmarContract.abi;
 
-  const contractAddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
   const connectWallet = useCallback(async () => {
     try {
